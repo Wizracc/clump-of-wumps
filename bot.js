7a7c7aa7aaa2256d89e6cbd25e0a97726e5d9e44
wizracc <wizracc@gmail.com>
2020-12-17 11:29:33 -0600

    updates

--- bot.js
+++ bot.js
@@ -1,26 +1,65 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
 const { token, prefix } = require("./config.json");
-const fs = require("fs");
-const puppeteer = require("puppeteer");
-
-let raw_data_set1 = fs.readFileSync(
-  "./set1-lite-en_us/en_us/data/set1-en_us.json"
-);
-let raw_data_set2 = fs.readFileSync(
-  "./set2-lite-en_us/en_us/data/set2-en_us.json"
-);
-const data1 = JSON.parse(raw_data_set1);
-const data2 = JSON.parse(raw_data_set2);
-const data = data1.concat(data2);
+const {
+  find_card,
+  short_results,
+  card_lookup,
+  long_results,
+  send_deck_image,
+  quick_deck,
+} = require("./helpers");
 
 client.on("ready", () => {
   console.log("I am ready!");
+  client.user.setActivity("for +deck CODE", { type: "WATCHING" });
 });
 
-client.on("message", (message) => {
-  // ignore non-prefixed commands, and ignore bots
-  if (!message.content.startsWith(prefix) || message.author.bot) return;
+client.on("message", async (message) => {
+  // ignore bots
+  if (message.author.bot) return;
+
+  // see if message has card searches in it
+  if (!message.content.startsWith(prefix)) {
+    const reg = /\[.*?\]/g;
+    const found = message.content.match(reg);
+    if (found !== null) {
+      var results = [];
+      var successes = [];
+      for (var string of found) {
+        const str = string.slice(1, -1);
+        const cards = find_card([str]);
+        if (cards.length > 0) {
+          const embeds = short_results(cards);
+          results.push(embeds);
+          successes.push(str);
+        }
+      }
+      var replyString = `Found ${successes.length} sets of card matches for`;
+      if (successes.length < 1) {
+        replyString = "";
+      } else if (successes.length === 1) {
+        replyString = `Found a match for ${successes[0]}`;
+      } else {
+        for (success of successes) {
+          replyString = replyString.concat(` ${success},`);
+        }
+        replyString = replyString.slice(0, -1);
+        replyString = replyString.concat(".");
+      }
+      if (replyString !== "") message.reply(replyString);
+      for (result of results) {
+        var index = 0;
+        await message.channel.send(result[index]);
+        if (result.length > 1) {
+          await message.channel.send(
+            "For associated cards, use +card *card name*"
+          );
+        }
+      }
+    }
+    return;
+  }
 
   const args = message.content.slice(prefix.length).split(" ");
   const command = args.shift().toLowerCase();
@@ -28,9 +67,13 @@ client.on("message", (message) => {
   /*
   fetch-commands
   */
-  if (command === "fetch-commands") {
-    let response_string = `The commands are as follows:\n
-${prefix}card-lookup: looks for string matches in titles and card text`;
+  if (command === "commands") {
+    let response_string = `The commands are as follows:
+\n\`[card name]\`: -(Usable in any part of a message, multiple times), shows the matching cards, if any
+\n\`${prefix}card-lookup overwhelm\`: -looks for basic string matches in titles or card text or keywords (multiple keywords must be in the same order as on the card)
+\n\`${prefix}card Draven\`: -shows the exact card, use reactions to view associated cards
+\n\`${prefix}deck-image DECKCODE\`: -shows an image of the deck, using https://decks.wizra.cc
+\n\`${prefix}deck DECKCODE\`: -shows a simplified decklist much more quickly than deck-image`;
     return message.channel.send(response_string);
   } else if (command === "card-lookup" || command === "card-search") {
     /*
@@ -41,13 +84,38 @@ ${prefix}card-lookup: looks for string matches in titles and card text`;
       message.channel.send("No results, please try another search");
     } else {
       var embeds = long_results(cards);
-      if (embeds.length > 5) {
+      if (embeds.length > 100) {
         message.channel.send(
           "Too many results, please use a longer search phrase."
         );
       } else {
-        for (var embed of embeds) {
-          message.channel.send(embed);
+        var index = 0;
+        const thisMessage = await message.channel.send(embeds[index]);
+        if (embeds.length > 1) {
+          await thisMessage.react("◀️");
+          await thisMessage.react("▶️");
+          const filter = (reaction, user) => {
+            return (
+              user.id !== client.user.id &&
+              (reaction.emoji.name === "▶️" || reaction.emoji.name === "◀️")
+            );
+          };
+          const collector = thisMessage.createReactionCollector(filter, {
+            time: 600000,
+          });
+          collector.on("collect", (reaction, user) => {
+            if (reaction.emoji.name === "▶️") {
+              if (index < embeds.length - 1) {
+                index++;
+                thisMessage.edit(embeds[index]);
+              }
+            } else if (reaction.emoji.name === "◀️") {
+              if (index > 0) {
+                index--;
+                thisMessage.edit(embeds[index]);
+              }
+            }
+          });
         }
       }
     }
@@ -61,257 +129,56 @@ ${prefix}card-lookup: looks for string matches in titles and card text`;
       message.channel.send("No results, plese try another card");
     } else {
       var embeds = short_results(cards);
-      for (var embed of embeds) {
-        message.channel.send(embed);
+      var replyMessage = "";
+      if (embeds.length > 1) {
+        replyMessage = `There are ${embeds.length} associated cards with \
+this search, use the ◀️▶️ buttons to navigate through the results after \
+the buttons appear. Click again to "reset" the button.`;
+      } else {
+        replyMessage = "There is only one card with this name.";
+      }
+      await message.channel.send(replyMessage);
+      var index = 0;
+      const thisMessage = await message.channel.send(embeds[index]);
+      if (embeds.length > 1) {
+        await thisMessage.react("◀️");
+        await thisMessage.react("▶️");
+        const filter = (reaction, user) => {
+          return (
+            user.id !== client.user.id &&
+            (reaction.emoji.name === "▶️" || reaction.emoji.name === "◀️")
+          );
+        };
+        const collector = thisMessage.createReactionCollector(filter, {
+          time: 600000,
+        });
+        collector.on("collect", (reaction, user) => {
+          if (reaction.emoji.name === "▶️") {
+            if (index < embeds.length - 1) {
+              index++;
+              thisMessage.edit(embeds[index]);
+            }
+          } else if (reaction.emoji.name === "◀️") {
+            if (index > 0) {
+              index--;
+              thisMessage.edit(embeds[index]);
+            }
+          }
+        });
       }
     }
     return;
-  } else if (command === "deck-image" || message.startsWith("!deck")) {
+  } else if (command === "deck-image") {
     /*
     deck image
     */
-    message.channel.send("Fetching deck image...");
-    fetchImage(args[0]).then((image) => {
-      if (image) {
-        var link = `https://decks.wizra.cc/${args[0]}`;
-        const embed = new Discord.MessageEmbed()
-          .setTitle("decks.wizra.cc")
-          .setURL(link)
-          .attachFiles([image])
-          .setImage(`attachment://${args[0]}.png`);
-        message.channel.send(embed);
-      } else {
-        message.channel.send("Not a valid deck code");
-      }
-    });
+    //send_deck_image(args[0], message);
+    message.channel.send("This command is temporarily disabled, please use +deck instead");
     return;
+  } else if (command === "deck") {
+    const embeds = quick_deck(args[0]);
+    message.channel.send(embeds[0]);
   }
 });
 
 client.login(token);
-
-function deck_translate(code) {
-
-}
-
-function card_lookup(args) {
-  var string = args.join(" ").toLowerCase();
-  var matches = [];
-  for (var card of data) {
-    if (
-      card.descriptionRaw.toLowerCase().includes(string) ||
-      card.name.toLowerCase().includes(string)
-    ) {
-      matches.push(card);
-    }
-  }
-  return matches.sort((a, b) => sort_cards(a, b));
-}
-
-function find_card(args) {
-  var string = args.join(" ").toLowerCase();
-  var matches = new Set();
-  for (var card of data.concat(data2)) {
-    if (card.name.toLowerCase() === string) {
-      matches.add(card);
-      for (var subcardCode of card.associatedCardRefs) {
-        var subcard = data.find((obj) => {
-          return obj.cardCode === subcardCode;
-        });
-        matches.add(subcard);
-      }
-    }
-  }
-  return [...matches].sort((a, b) => sort_cards(a, b, string));
-}
-
-/**
- * long_results returns an array of embeds using the given array.
- *
- * This method takes in an array of card objects filtered from
- * the datadragon json, and creates 1 or more embed objects
- * that lists the names, cost, and rarity
- *
- * @param {[objectVar]} cards an array of card objects
- *
- * @return {[objectVar]}      an array of embed objects
- */
-function long_results(cards) {
-  var embeds = [];
-  var freshEmbed = new Discord.MessageEmbed()
-    .setTitle("Search Results")
-    .setFooter(
-      `To view an individual card, use ${prefix}card or ${prefix}find-card`
-    );
-  var s = " ";
-  for (var card of cards) {
-    var newLine = "";
-    newLine += `${rarity_to_emoji(card.rarity)} `;
-    newLine += `__**${card.cost}**__ `;
-    newLine += `${card.name}\n`;
-
-    s += newLine;
-
-    if (s.length + newLine.length >= 1000) {
-      freshEmbed.addFields({
-        name: "[Rarity] __**Cost**__ Name",
-        value: s,
-        inline: true,
-      });
-      embeds.push(freshEmbed);
-
-      s = " ";
-      freshEmbed = new Discord.MessageEmbed()
-        .setTitle(`Search Results ${embeds.length + 1}`)
-        .setFooter(
-          `To view an individual card, use ${prefix}card or ${prefix}find-card`
-        );
-    }
-  }
-
-  freshEmbed.addFields({
-    name: "[Rarity] __**Cost**__ Name",
-    value: s,
-    inline: true,
-  });
-
-  embeds.push(freshEmbed);
-  return embeds;
-}
-
-/*
-short_results
-
-returns an array of embeds of all the cards
-*/
-function short_results(cards) {
-  var embeds = [];
-  for (card of cards) {
-    var freshEmbed = new Discord.MessageEmbed()
-      .setTitle(`${region_to_emoji(card.regionRef)} ${card.name}`)
-      .setThumbnail(card.assets[0].fullAbsolutePath)
-      .addField("Cost", `${card.cost}`, true)
-      .addField("Region", `${card.region}`, true)
-      .addField(
-        "Keywords",
-        `${card.keywords.length > 0 ? card.keywords.join("\n") : "(none)"}`,
-        true
-      )
-      .addField(
-        "Description",
-        `${card.descriptionRaw.length > 0 ? card.descriptionRaw : "(none)"}`
-      );
-    if (card.type.toLowerCase() === "unit") {
-      freshEmbed
-        .addField("Attack", `${card.attack}`, true)
-        .addField("Health", `${card.health}`, true);
-    }
-    freshEmbed.setImage(card.assets[0].gameAbsolutePath);
-    embeds.push(freshEmbed);
-  }
-  return embeds;
-}
-
-/*
-  region to emoji
-*/
-function region_to_emoji(region) {
-  const regions = {
-    "": "<:iconall:708929175045668934>",
-    none: "<:iconall:708929175045668934>",
-    bilgewater: "<:iconbilgewater:708929174777364572>",
-    demacia: "<:icondemacia:708929174995337227>",
-    freljord: "<:iconfreljord:708929175263641611>",
-    ionia: "<:iconionia:708929175225892985>",
-    noxus: "<:iconnoxus:708929175775608873>",
-    piltoverzaun: "<:iconpiltoverzaun:708929175905632297>",
-    shadowisles: "<:iconshadowisles:708929175943118919>",
-  };
-  return regions[region.toLowerCase()];
-}
-
-/*
-  rarity to emoji
-
-  uses a list of custom emojis to transate rarity names to icon
-*/
-function rarity_to_emoji(rarity) {
-  const rarities = {
-    common: "🟩",
-    rare: "🟦",
-    epic: "🟪",
-    champion: "🟧",
-    "": "❎",
-    none: "❎",
-  };
-  return rarities[rarity.toLowerCase()];
-}
-
-function sort_cards(a, b, string = "") {
-  if (a.name.toLowerCase() === string && b.name.toLowerCase() !== string) {
-    return -1;
-  } else if (b.name.toLowerCase() === string && a.name.toLowerCase !== string) {
-    return 1;
-  } else if (a.name === b.name) {
-    return a.cardCode > b.cardCode ? 1 : -1;
-  } else {
-    return a.name > b.name ? 1 : -1;
-  }
-}
-
-async function fetchImage(code) {
-  let image = "";
-  let link = "";
-  let valid = true;
-
-  if (fs.existsSync(`${__dirname}/${code}.png`)) {
-    link = `https://decks.wizra.cc/${code}`;
-    image = `${__dirname}/${code}.png`;
-  } else {
-    /* enable this when testing on windows
-
-    */
-    const browser = await puppeteer.launch({
-      ignoreDefaultArgs: ["--disable-extensions"],
-    });
-    /* enable this on production
-    const browser = await puppeteer.launch({
-      executablePath: "chromium-browser",
-    });
-    */
-    const page = await browser.newPage();
-    await page.setViewport({
-      width: 1000,
-      height: 2000,
-      deviceScaleFactor: 1,
-    });
-    await page.goto(`https://decks.wizra.cc/${code}`, {
-      waitUntil: "networkidle2",
-    });
-    const rect = await page.evaluate(() => {
-      const cardArr = document.querySelectorAll(".Minicard");
-      const cardCount = cardArr.length;
-      const element = document.querySelector("div.cards");
-      const { x, y, width, height } = element.getBoundingClientRect();
-      return { left: x, top: y, width, height, cardCount };
-    });
-    if (rect.cardCount !== 0) {
-      await page.screenshot({
-        path: `${code}.png`,
-        clip: {
-          x: rect.left,
-          y: rect.top,
-          width: rect.width,
-          height: 31 * rect.cardCount,
-        },
-      });
-      link = `https://decks.wizra.cc/${code}`;
-      image = `${__dirname}/${code}.png`;
-    } else {
-      valid = false;
-    }
-    await browser.close();
-  }
-
-  return valid ? image : false;
-}
---
+++ helpers.js
@@ -0,0 +1,450 @@
+const Discord = require("discord.js");
+const { prefix } = require("./config.json");
+const fs = require("fs");
+const puppeteer = require("puppeteer");
+const { DeckEncoder } = require("runeterra");
+
+let raw_data_set1 = fs.readFileSync(
+  "./set1-lite-en_us/en_us/data/set1-en_us.json"
+);
+let raw_data_set2 = fs.readFileSync(
+  "./set2-lite-en_us/en_us/data/set2-en_us.json"
+);
+let raw_data_set3 = fs.readFileSync(
+  "./set3-lite-en_us/en_us/data/set3-en_us.json"
+);
+const data1 = JSON.parse(raw_data_set1);
+const data2 = JSON.parse(raw_data_set2);
+const data3 = JSON.parse(raw_data_set3);
+const data12 = data1.concat(data2);
+const data = data12.concat(data3);
+
+function deck_translate(code) {
+  try {
+    const rawDeck = DeckEncoder.decode(code);
+    var deck = {
+      regions: {
+        bilgewater: [],
+        demacia: [],
+        freljord: [],
+        ionia: [],
+        noxus: [],
+        piltoverzaun: [],
+        shadowisles: [],
+        targon: [],
+      },
+      types: {
+        champion: [],
+        unit: [],
+        spell: [],
+      },
+    };
+    for (const card of rawDeck) {
+      var cardCode = card.code;
+      cardCode = cardCode.replace("undefined", "MT");
+      const raw = { ...data.find((o) => o.cardCode === cardCode) };
+      const cardInfo = {
+        region: raw.regionRef.toLowerCase(),
+        name: raw.name,
+        cost: raw.cost,
+        count: card.count,
+        code: raw.cardCode,
+        type: raw.supertype === "Champion" ? "Champion" : raw.type,
+        rarity: raw.rarity.toLowerCase(),
+      };
+      deck.regions[cardInfo.region.toLowerCase()].push(cardInfo);
+      deck.types[cardInfo.type.toLowerCase()].push(cardInfo);
+    }
+    return deck;
+  } catch (error) {
+    console.error(error);
+    return null;
+  }
+}
+
+function quick_deck(code) {
+  const data = quick_deck_data(code);
+  if (data !== null) {
+    var embeds = [];
+    var regionEmbed = new Discord.MessageEmbed()
+      .setTitle(data[0].title)
+      .setFooter(
+        `If you see any problems, message Wizracc#0574 to let them know`
+      );
+    for (region of data[0].regions) {
+      var regionName = regionRef_to_regionName(region.name);
+      var emoji = region_to_emoji(region.name);
+      regionEmbed.addField(`${emoji} **${regionName}**`, region.cards, true);
+    }
+    embeds.push(regionEmbed);
+    var typeEmbed = new Discord.MessageEmbed().setTitle(data[1].title);
+    for (type in data[1].types) {
+      if (type.length !== "") {
+        typeEmbed.addField(type, data[1].types[type], true);
+      }
+    }
+    embeds.push(typeEmbed);
+    return embeds;
+  } else {
+    return null;
+  }
+}
+
+function cards_sort_cost_name(a, b) {
+  if (a.cost < b.cost) {
+    return -1;
+  } else if (a.cost > b.cost) {
+    return 1;
+  } else {
+    return cards_sort_name(a, b);
+  }
+  return 0;
+}
+
+function cards_sort_name(a, b) {
+  if (a.name.toUpperCase() < b.name.toUpperCase()) {
+    return -1;
+  }
+  if (a.name.toUpperCase() > b.name.toUpperCase()) {
+    return 1;
+  }
+  return 0;
+}
+
+function quick_deck_data(code) {
+  const deck = deck_translate(code);
+  if (deck !== null) {
+    var embed1_data = {
+      title: "",
+      regions: [],
+    };
+    var embed2_data = {
+      title: "",
+      types: {
+        champion: "",
+        unit: "",
+        spell: "",
+      },
+    };
+    var title = "";
+
+    var index = 0;
+
+    for (region in deck.regions) {
+      if (deck.regions[region].length > 0) {
+        embed1_data.regions.push({ name: region, cards: "" });
+        let region_cards = deck.regions[region];
+        region_cards.sort(cards_sort_cost_name);
+        for (card of region_cards) {
+          embed1_data.regions[index].cards = embed1_data.regions[
+            index
+          ].cards.concat(
+            `\n${rarity_to_emoji(card.rarity)}` +
+              ` **${card.count}** ${card.name}`
+          );
+        }
+        index++;
+      }
+    }
+
+    for (type in deck.types) {
+      if (deck.types[type].length > 0) {
+        for (card of deck.types[type]) {
+          embed2_data.types[type] = embed2_data.types[type].concat(
+            `\n**${card.count}** ${card.name}`
+          );
+        }
+      } else {
+        delete embed2_data.types[type];
+      }
+    }
+
+    if ("champion" in embed2_data.types) {
+      for (champ of deck.types.champion) {
+        title = title.concat(champ.name + " & ");
+      }
+    } else {
+      for (region of embed1_data.regions) {
+        var regionName = regionRef_to_regionName(region.name);
+        title = title.concat(regionName + " & ");
+      }
+    }
+    embed1_data.title = title.slice(0, -3);
+    embed2_data.title = title.slice(0, -3);
+
+    return [embed1_data, embed2_data];
+  } else {
+    return null;
+  }
+}
+
+function regionRef_to_regionName(regionRef) {
+  const obj = {
+    bilgewater: "Bilgewater",
+    demacia: "Demacia",
+    freljord: "Freljord",
+    ionia: "Ionia",
+    noxus: "Noxus",
+    piltoverzaun: "Piltover & Zaun",
+    shadowisles: "Shadow Isles",
+    targon: "Mount Targon",
+    none: "<no region>",
+  };
+  return obj[regionRef.toLowerCase()];
+}
+
+function send_deck_image(code, message) {
+  message.channel.send("Fetching deck image...");
+  fetch_image(code).then((image) => {
+    if (image) {
+      var link = `https://decks.wizra.cc/${code}`;
+      const embed = new Discord.MessageEmbed()
+        .setTitle("decks.wizra.cc")
+        .setURL(link)
+        .attachFiles([image])
+        .setImage(`attachment://${code}.png`);
+      message.channel.send(embed);
+    } else {
+      message.channel.send("Not a valid deck code");
+    }
+  });
+}
+
+function card_lookup(args) {
+  var string = args.join(" ").toLowerCase();
+  var matches = [];
+  for (var card of data) {
+    if (
+      card.descriptionRaw.toLowerCase().includes(string) ||
+      card.name.toLowerCase().includes(string) ||
+      card.keywords.join(" ").toLowerCase().includes(string)
+    ) {
+      matches.push(card);
+    }
+  }
+  return matches.sort((a, b) => sort_cards(a, b));
+}
+
+function find_card(args) {
+  var string = args.join(" ").toLowerCase();
+  var matches = new Set();
+  for (var card of data) {
+    if (card.name.toLowerCase().trim() === string) {
+      matches.add(card);
+      for (var subcardCode of card.associatedCardRefs) {
+        var subcard = data.find((obj) => {
+          return obj.cardCode === subcardCode;
+        });
+        matches.add(subcard);
+      }
+    }
+  }
+  var matchArray = [...matches].sort((a, b) => sort_cards(a, b, string));
+  if (
+    matchArray[0] !== undefined &&
+    matchArray[0].name.toLowerCase() === "overgrown snapvine"
+  ) {
+    matchArray = Array(10).fill(matchArray[0]);
+  }
+  return matchArray;
+}
+
+/**
+ * long_results returns an array of embeds using the given array.
+ *
+ * This method takes in an array of card objects filtered from
+ * the datadragon json, and creates 1 or more embed objects
+ * that lists the names, cost, and rarity
+ *
+ * @param {[objectVar]} cards an array of card objects
+ *
+ * @return {[objectVar]}      an array of embed objects
+ */
+function long_results(cards) {
+  var embeds = [];
+  var freshEmbed = new Discord.MessageEmbed()
+    .setTitle("Search Results 1")
+    .setFooter(
+      `To view an individual card, use ${prefix}card, \nuse reactions to navigate multiple pages \n(un-react to reuse buttons, if no buttons present, only 1 page)`
+    );
+  var s = " ";
+  for (var card of cards) {
+    var newLine = "";
+    newLine += `${rarity_to_emoji(card.rarity)} `;
+    newLine += `__**${card.cost}**__ `;
+    newLine += `${card.name}\n`;
+
+    s += newLine;
+
+    if (s.length + newLine.length >= 600) {
+      freshEmbed.addFields({
+        name: "[Rarity] __**Cost**__ Name",
+        value: s,
+        inline: true,
+      });
+      embeds.push(freshEmbed);
+
+      s = " ";
+      freshEmbed = new Discord.MessageEmbed()
+        .setTitle(`Search Results ${embeds.length + 1}`)
+        .setFooter(
+          `To view an individual card, use ${prefix}card, \nuse reactions to navigate multiple pages \n(un-react to reuse buttons)`
+        );
+    }
+  }
+
+  freshEmbed.addFields({
+    name: "[Rarity] __**Cost**__ Name",
+    value: s,
+    inline: true,
+  });
+
+  embeds.push(freshEmbed);
+  return embeds;
+}
+
+/*
+short_results
+
+returns an array of embeds of all the cards
+*/
+function short_results(cards) {
+  var embeds = [];
+  for (card of cards) {
+    var freshEmbed = new Discord.MessageEmbed()
+      .setTitle(`${region_to_emoji(card.regionRef)} ${card.name}`)
+      .setThumbnail(card.assets[0].fullAbsolutePath)
+      .addField("Cost", `${card.cost}`, true)
+      .addField("Region", `${card.region}`, true)
+      .addField(
+        "Keywords",
+        `${card.keywords.length > 0 ? card.keywords.join("\n") : "(none)"}`,
+        true
+      )
+      .addField(
+        "Description",
+        `${card.descriptionRaw.length > 0 ? card.descriptionRaw : "(none)"}`
+      );
+    if (card.type.toLowerCase() === "unit") {
+      freshEmbed
+        .addField("Attack", `${card.attack}`, true)
+        .addField("Health", `${card.health}`, true);
+    }
+    freshEmbed.setImage(card.assets[0].gameAbsolutePath);
+    embeds.push(freshEmbed);
+  }
+  return embeds;
+}
+
+/*
+  region to emoji
+*/
+function region_to_emoji(region) {
+  const regions = {
+    "": "<:iconall:708929175045668934>",
+    none: "<:iconall:708929175045668934>",
+    bilgewater: "<:iconbilgewater:708929174777364572>",
+    demacia: "<:icondemacia:708929174995337227>",
+    freljord: "<:iconfreljord:708929175263641611>",
+    ionia: "<:iconionia:708929175225892985>",
+    noxus: "<:iconnoxus:708929175775608873>",
+    piltoverzaun: "<:iconpiltoverzaun:708929175905632297>",
+    shadowisles: "<:iconshadowisles:708929175943118919>",
+    targon: "<:icontargon:748257274736279730>",
+  };
+  return regions[region.toLowerCase()];
+}
+
+/*
+  rarity to emoji
+
+  uses a list of custom emojis to transate rarity names to icon
+*/
+function rarity_to_emoji(rarity) {
+  const rarities = {
+    common: "🟩",
+    rare: "🟦",
+    epic: "🟪",
+    champion: "🟧",
+    "": "❎",
+    none: "❎",
+  };
+  return rarities[rarity.toLowerCase()];
+}
+
+function sort_cards(a, b, string = "") {
+  if (a.name.toLowerCase() === b.name.toLowerCase()) {
+    return a.cardCode > b.cardCode ? 1 : -1;
+  } else if (
+    a.name.toLowerCase() === string &&
+    b.name.toLowerCase() !== string
+  ) {
+    return -1;
+  } else if (b.name.toLowerCase() === string && a.name.toLowerCase !== string) {
+    return 1;
+  } else {
+    return a.name > b.name ? 1 : -1;
+  }
+}
+
+async function fetch_image(code) {
+  let image = "";
+  let valid = true;
+
+  if (fs.existsSync(`${__dirname}/${code}.png`)) {
+    image = `${__dirname}/${code}.png`;
+  } else {
+    /* enable this when testing on windows
+    const browser = await puppeteer.launch({
+      ignoreDefaultArgs: ["--disable-extensions"],
+    });
+    /* */
+    /* enable this on production */
+    const browser = await puppeteer.launch({
+      executablePath: "chromium-browser",
+    });
+    /* */
+    const page = await browser.newPage();
+    await page.setViewport({
+      width: 1000,
+      height: 2000,
+      deviceScaleFactor: 1,
+    });
+    await page.goto(`https://decks.wizra.cc/${code}`, {
+      waitUntil: "networkidle2",
+    });
+    const rect = await page.evaluate(() => {
+      const cardArr = document.querySelectorAll(".Minicard");
+      const cardCount = cardArr.length;
+      const element = document.querySelector("div.cards");
+      const { x, y, width, height } = element.getBoundingClientRect();
+      return { left: x, top: y, width, height, cardCount };
+    });
+    if (rect.cardCount !== 0) {
+      await page.screenshot({
+        path: `${code}.png`,
+        clip: {
+          x: rect.left,
+          y: rect.top,
+          width: rect.width,
+          height: 31 * rect.cardCount,
+        },
+      });
+      image = `${__dirname}/${code}.png`;
+    } else {
+      valid = false;
+    }
+    await browser.close();
+  }
+
+  return valid ? image : false;
+}
+
+module.exports = {
+  find_card,
+  short_results,
+  card_lookup,
+  long_results,
+  send_deck_image,
+  quick_deck,
+};
